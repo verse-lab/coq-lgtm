f305ba1740d7729eca22b71c905aff4e6d54aeed
volodeyka <vovaglad00@gmail.com>
2023-10-26 15:20:27 +0800

    factor out unrelated code from CSR.v

--- CSR.v
+++ CSR.v
@@ -1,7 +1,7 @@
 Set Implicit Arguments.
 From SLF Require Import LabType Fun LibSepFmap Sum.
 From SLF Require Import LibWP LibSepSimpl LibSepReference LibSepTLCbuffer.
-From SLF Require Import Struct Loops Unary_IndexWithBounds SV2 Subst.
+From SLF Require Import Struct Loops Unary_IndexWithBounds SV2 Subst NTriple.
 From mathcomp Require Import ssreflect ssrfun zify.
 Hint Rewrite conseq_cons' : rew_listx.
 
@@ -78,13 +78,7 @@ Definition csrij :=
     let "lb" = read_array rowptr i in
     let "ii" = i + 1 in
     let "rb" = read_array rowptr "ii" in
-    (* let k = indexf "lb" "rb" j colind in
-    let c = k < "rb" in
-    if c
-    then 
-      let "tmp" = read_array mval k in "tmp"
-    else 0 *)
-    sv.get j colind mval "lb" "rb"
+      sv.get j colind mval "lb" "rb"
 }>.
 
 Lemma csrij_spec_in {D : Type} `{Inhab D} (x_mval x_colind x_rowptr : loc) (i j : int) d : 
@@ -155,12 +149,6 @@ Definition csrsum :=
   <{
   fun mval colind rowptr =>
   let s = ref 0 in
-  (* 
-  for i <- [0, N] {
-    let x = mval[i] in 
-    s += x
-  }; 
-  *)
   for i <- [0, Nrow] {
     let "lb" = read_array rowptr i in
     let "ii" = i + 1 in
@@ -176,185 +164,13 @@ Tactic Notation "seclocal_fold" :=
     -/(For_aux _ _) 
     -/(For _ _ _) //=.
 
-
-Lemma htriple_letu2 {D : Type} (fs fs' : fset _) H Q' Q ht ht1 ht2 htpre ht' x
-  (Hdj : disjoint fs fs')
-  (Hhtpre : forall d, indom fs d -> htpre d = ht1 d)
-  (Hhtpre' : forall d, indom fs' d -> htpre d = ht' d)
-  (Htppre : htriple (fs \u fs') htpre H Q') (* hv? *)
-  (Hht : forall d, indom fs d -> ht d = trm_let (x d) (ht1 d) (ht2 d))
-  (Hht' : forall d, indom fs' d -> ht d = ht' d)
-  (Htp2 : forall hv, htriple fs (fun d => subst (x d) (hv d) (ht2 d)) (Q' hv) (fun hr => Q (uni fs' hv hr))) :
-  (* (Hcong : forall hv1 hv2, (forall d, indom (fs \u fs') d -> hv1 d = hv2 d) -> 
-    Q hv1 ==> Q hv2) : *)
-  @htriple D (fs \u fs') ht H Q.
-Proof using All.
-  apply wp_equiv.
-  rewrite -> union_comm_of_disjoint. 2: apply Hdj.
-  rewrite <- wp_union. 2: rewrite -> disjoint_comm; apply Hdj.
-  rewrite -> wp_ht_eq with (ht2:=ht').
-  2: apply Hht'.
-  rewrite -> wp_ht_eq with (ht2:=htpre).
-  2: introv HH; rewrite -> Hhtpre'; try reflexivity; try apply HH.
-  apply wp_equiv.
-
-  eapply htriple_conseq.
-  3:{ 
-    hnf. intros v. 
-    eapply himpl_trans.
-    1: apply wp_let.
-    rewrite -> wp_ht_eq with (ht1:=ht) (ht2:=fun d => trm_let (x d) (ht1 d) (ht2 d)).
-    2: apply Hht.
-    apply himpl_refl.
-  }
-  1:{ 
-    apply wp_equiv.
-    eapply wp_conseq. hnf. intros.
-    match goal with |- himpl _ (wp ?fs _ ?ff) => 
-      eapply himpl_trans with (H2:=wp fs htpre ff) end.
-    1: apply himpl_refl.
-    rewrite -> wp_ht_eq with (ht1:=ht1) (ht2:=htpre).
-    2: introv HH; rewrite -> Hhtpre; try reflexivity; try apply HH.
-    apply himpl_refl.
-  }
-  apply wp_equiv in Htppre.
-  rewrite -> union_comm_of_disjoint in Htppre. 2: apply Hdj.
-  rewrite <- wp_union in Htppre. 2: rewrite -> disjoint_comm; apply Hdj.
-  eapply himpl_trans.
-  1: apply Htppre.
-  apply wp_conseq.
-  hnf. intros. apply wp_conseq.
-  hnf. intros.
-  (*   have {}Htppre: H ==> wp fs' htpre (fun=> wp fs htpre Q').
-  { apply: himpl_trans; last apply:wp_union2; autos*. }
-  (* 2: rewrite -> disjoint_comm; apply Hdj. *)
-  eapply himpl_trans.
-  1: apply Htppre.
-  apply wp_conseq.
-  hnf. intros. apply wp_conseq.
-  hnf. intros. 
-  xapp=> hv.
-  move=> ?; apply:applys_eq_init; f_equal; extens=> ?; rewrite /uni.
-  do? case:classicT=> //. *)
-  erewrite wp_ht_eq.
-  { xapp=> hv. 
-    move=> ?; apply:applys_eq_init; f_equal; extens=> ?; rewrite /uni.
-    do? case:classicT=> //. } 
-  move=> ? IN /=. 
-  rewrite /uni; case: classicT=> // /(disjoint_inv_not_indom_both _ IN).
-  case; autos*.
-Qed.
-
-Lemma ntriple_letu2 {D : Type} (s : _) H Q' Q fsht x
-  (t1 t2 : trm) (i : int)
-  (Htppre : 
-    ntriple H
-      (Lab (pair i 0) (FH (`{s}) (fun=>t1)) :: 
-       fsht)
-    Q')
-  (Htp2 : forall hv, 
-    htriple (label (Lab (pair i 0) (`{s}))) (fun d => subst x (hv[`i](s)) t2) 
-      (Q' hv) (fun hr => Q (uni (fset_of fsht) hv hr)))
-  :
-  ~ has_lab fsht (i,0) ->
-  @ntriple D H
-    (Lab (pair i 0) (FH (`{s}) (fun d => trm_let x t1 t2)) :: 
-    fsht)
-    Q.
-Proof using All.
-  (* move/hasnt_lab. *)
-  move=> HNL.
-  unfold ntriple, nwp.
-  simpl fset_of.
-  erewrite -> wp_ht_eq.
-  1: apply wp_equiv.
-  1: eapply htriple_letu2 with 
-    (ht1:=fun=> t1)
-    (ht2:=fun=> t2)
-    (htpre:=uni (label (Lab (pair i 0) (`{s}))) (fun=> t1)
-      (htrm_of fsht))
-    (ht:=uni (label (Lab (pair i 0) (`{s}))) (fun d => trm_let x t1 t2)
-      (htrm_of fsht))
-    (ht':=htrm_of fsht).
-  { rewrite (hasnt_lab _ _ HNL); exact/fset_htrm_label_remove_disj. }
-  { intros. unfold uni. case_if; try reflexivity. contradiction. }
-  { move=> ?; rewrite (hasnt_lab _ _ HNL) /uni; case: classicT=> //.
-    move=>/(@disjoint_inv_not_indom_both _ _ _ _ _)/[apply]-[].
-    exact/fset_htrm_label_remove_disj. }
-  2:{
-    intros. unfold uni. case_if; try reflexivity. contradiction.
-  }
-  2:{ move=> ?; rewrite (hasnt_lab _ _ HNL) /uni; case: classicT=> //.
-    move=>/(@disjoint_inv_not_indom_both _ _ _ _ _)/[apply]-[].
-    exact/fset_htrm_label_remove_disj. }
-  3:{ case=> *; by rewrite /uni /= indom_label_eq. }
-  2: { move=> hr /=. 
-    rewrite -wp_equiv label_single wp_single -label_single wp_equiv.
-    apply/Htp2. }
-  unfold ntriple, nwp in Htppre.
-  simpl fset_of in Htppre.
-  apply wp_equiv.
-  erewrite -> wp_ht_eq in Htppre.
-  1: apply Htppre.
-
-  intros d H0. destruct d as (ll, d).
-  rewrite -> indom_union_eq, -> ! indom_label_eq in H0. 
-  unfold htrm_of, uni. rewrite ! indom_label_eq. simpl. repeat case_if; try eqsolve.
-Qed.
-
-Lemma xletu2 {D : Type} (s : _) H Q' Q fsht x
-  (t1 t2 : trm) (i : int)
-  (Htppre : 
-    ntriple H
-      (Lab (pair i 0) (FH (`{s}) (fun=>t1)) :: 
-       fsht)
-    Q')
-  (Htp2 : forall hv, 
-    htriple (label (Lab (pair i 0) (`{s}))) (fun d => subst x (hv[`i](s)) t2) 
-      (Q' hv) (fun hr => Q (lab_fun_upd hr hv (i,0))))
-  :
-  ~ has_lab fsht (i,0) ->
-  @ntriple D H
-    (Lab (pair i 0) (FH (`{s}) (fun d => trm_let x t1 t2)) :: 
-    fsht)
-    Q.
-Proof.
-  move=> ?; apply/ntriple_letu2; eauto=> hv.
-  rewrite -wp_equiv -/(fs_of (FH (`{s}) (fun=> subst x (hv[`i](s)) t2))).
-  rewrite -/(ht_of (FH (`{s}) (fun=> subst x (hv[`i](s)) t2))) xnwp1_lemma /=.
-  rewrite -xnwp1_lemma/=; apply:himpl_trans; last exact/wp_hv.
-  xapp=> hr. xsimpl (lab_fun_upd hr hv (i,0))=> ?; apply:applys_eq_init; f_equal.
-  extens=> -[??] /=; rewrite /uni; case: classicT.
-  { erewrite (hasnt_lab fsht); eauto=> /(@indom_remove _ _ _ _ _)=> lN.
-    case E: (lab_eqb _ _)=> //; move: lN; rewrite -lab_eqbE E //. }
-  case: classicT=> //; rewrite indom_label_eq indom_single_eq=> -[]->.
-  by rewrite eqbxx.
-Qed.
-
-Lemma xnapp_lemma' : forall {D : Type} fs_ht Q1 H1 H Q,
-  ntriple H1 fs_ht Q1 ->
-  H ==> H1 \* (Q1 \--* protect Q) ->
-  @ntriple D H fs_ht Q.
-Proof using.
-  introv tE M W.
-  apply/xapp_lemma; autos*.
-  by rewrite -wp_equiv.
-Qed.
-
 Lemma wp_prod_single {A B : Type} s fs Q ht (l : labType):
   @wp (labeled (A * B)) (label (Lab l (`{s} \x fs))) ht Q = 
   @wp (labeled (A * B)) (label (Lab l (`{s} \x fs))) (fun ld => ht(Lab l (s, (eld ld).2))) Q.
 Proof.
 Admitted.
 
-Tactic Notation "xnapp" constr(E) := 
-  rewrite -> ?hbig_fset_hstar;
-  apply/xletu2=> //=; [eapply xnapp_lemma'; 
-       [eapply E|
-         let hp := fresh "hp" in 
-         let HE := fresh "HE" in 
-        remember hpure as hp eqn:HE;
-       xapp_simpl=> ?; rewrite HE; exact: himpl_refl]|]; eauto; simpl.
+
 
 Lemma in_interval_list {A : Type} (l : list A) lb rb x: 
    In x (list_interval lb rb l) -> In x l.
