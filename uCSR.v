405ffd9a4941619b3fca33b5d82d16f0b5a4ea79
volodeyka <vovaglad00@gmail.com>
2023-10-27 14:29:32 +0800

    ucsr spec progress

---
+++ uCSR.v
@@ -0,0 +1,173 @@
+Set Implicit Arguments.
+From SLF Require Import LabType Fun LibSepFmap Sum.
+From SLF Require Import LibWP LibSepSimpl LibSepReference LibSepTLCbuffer Unary.
+From SLF Require Import Struct Loops Unary_IndexWithBounds SV2 Subst NTriple Loops2 Struct2.
+From mathcomp Require Import ssreflect ssrfun zify.
+Hint Rewrite conseq_cons' : rew_listx.
+
+Open Scope Z_scope.
+
+Definition to_int (v : val) : int := 
+  match v with 
+  | val_int i => i 
+  | _ => 0
+  end.
+
+Coercion to_int : val >-> Z.
+
+(* Module Export AD := WithUnary(Int2Dom). *)
+
+Module unordered_csr.
+
+Import sv.
+
+Section unordered_csr.
+
+Notation "'mval'" := ("m_val":var) (in custom trm at level 0) : trm_scope.
+Notation "'midx'" := ("m_idx":var) (in custom trm at level 0) : trm_scope.
+Notation "'colind'" := ("col_ind":var) (in custom trm at level 0) : trm_scope.
+Notation "'rowptr'" := ("row_ptr":var) (in custom trm at level 0) : trm_scope.
+Notation "'dvec'" := ("d_vec":var) (in custom trm at level 0) : trm_scope.
+Notation "'lb'" := ("l_b":var) (in custom trm at level 0) : trm_scope.
+Notation "'rb'" := ("r_b":var) (in custom trm at level 0) : trm_scope.
+Notation "'i''" := ("i_'":var) (in custom trm at level 0) : trm_scope.
+
+Notation "'for' i <- '[' Z ',' N ']' '{' t '}'"  :=
+  (For Z N <{ fun_ i => t }>)
+  (in custom trm at level 69,
+    Z, N, i at level 0,
+    format "'[' for  i  <-  [ Z ','  N ] ']'  '{' '/   ' '[' t  '}' ']'") : trm_scope.
+
+Import List Vars.
+
+Context (mval midx colind rowptr : list int).
+Context (N Nrow Ncol Nidx : int).
+Hypothesis Nrow_nonneg : 0 <= Nrow.
+Hypothesis len_mval : length mval = N :> int.
+Hypothesis len_midx : length midx = Nidx :> int.
+Hypotheses Nidx_leq_Ncol : Nidx <= Nrow.
+Hypothesis len_colind : length colind = N :> int.
+Hypothesis len_rowptr : length rowptr = Nidx + 1 :> int.
+Hypothesis colind_leq : forall x, In x colind -> 0 <= x < Ncol.
+Hypothesis rowptr_first : List.nth (abs 0) rowptr 0 = 0.
+Hypothesis rowptr_last : List.nth (abs Nrow) rowptr 0 = length midx.
+Hypothesis rowptr_weakly_sorted : forall (i j : int), 
+  (0 <= i <= Nidx) -> 
+  (0 <= j <= Nidx) -> 
+  (i <= j) -> 
+  (rowptr[i] <= rowptr[j]).
+
+Definition colind_seg (row : int) :=
+  list_interval (abs rowptr[row]) (abs rowptr[row+1]) colind.
+
+Hypothesis nodup_eachcol : forall x : int, 0 <= x < Nidx -> NoDup (colind_seg x).
+
+(* FIXME: rowptr may not be strictly increasing? so the existing definition of increasing list 
+    may not work, currently use these instead *)
+
+Tactic Notation "seclocal_solver" :=
+  first [ assumption 
+    | rewrite -/(colind_seg _); now apply nodup_eachcol
+    | (* for rowptr[?] <= N *)
+      try rewrite -> len_colind; try rewrite -> len_mval; 
+      try rewrite <- rowptr_last; apply rowptr_weakly_sorted; math
+    | (* for 0 <= rowptr[?] *)
+      rewrite <- rowptr_first at 1; apply rowptr_weakly_sorted; math
+    | (* for 0 <= rowptr[i] <= rowptr[i + 1] *)
+      split; [ rewrite <- rowptr_first at 1 | ]; apply rowptr_weakly_sorted; math
+    | idtac ]; auto.
+
+Definition indexf := index_bounded.func Nidx.
+
+Definition get := 
+<{
+  fun mval midx colind rowptr i => fun j =>
+      let i = indexf i midx in
+      if i < Nidx then
+        let lb = read_array rowptr i in
+        let i' = i + 1 in
+        let rb = read_array rowptr i' in
+          sv.get j colind mval lb rb
+      else 0
+}>.
+
+Definition sum := 
+  <{
+  fun mval midx colind rowptr =>
+  let s = ref 0 in
+  for i <- [0, Nidx] {
+    let i = read_array midx i in
+    let lb = read_array rowptr i in
+    let i' = i + 1 in
+    let rb = read_array rowptr i' in
+    let x = sv.sum colind mval lb rb in 
+      s += x
+  };
+  ! s
+}>.
+
+Tactic Notation "seclocal_fold" := 
+  rewrite ?label_single ?wp_single
+    -/(For_aux _ _) 
+    -/(For _ _ _) //=.
+
+
+Lemma in_interval_list {A : Type} (l : list A) lb rb x: 
+   In x (list_interval lb rb l) -> In x l.
+Proof.
+Admitted.
+
+Arguments in_interval_list {_ _ _ _ _}.
+
+Local Notation Dom := (int * int)%type.
+Local Notation D := (labeled (int * int)).
+Definition eld := @LibWP.eld (int * int)%type.
+Coercion eld : D >-> Dom.
+
+Lemma sum_spec `{Inhab D} (x_mval x_colind x_rowptr x_midx : loc) : 
+  {{ arr(x_mval, mval)⟨1, (0,0)⟩ \*
+     arr(x_midx, midx)⟨1, (0,0)⟩ \*
+     arr(x_colind, colind)⟨1, (0,0)⟩ \*
+     arr(x_rowptr, rowptr)⟨1, (0,0)⟩ \*
+     (\*_(i <- `[0, Nrow] \x `[0, Ncol]) arr(x_mval, mval)⟨2, i⟩) \*
+     (\*_(i <- `[0, Nrow] \x `[0, Ncol]) arr(x_midx, midx)⟨2, i⟩) \*
+     (\*_(i <- `[0, Nrow] \x `[0, Ncol]) arr(x_colind, colind)⟨2, i⟩) \*
+     (\*_(i <- `[0, Nrow] \x `[0, Ncol]) arr(x_rowptr, rowptr)⟨2, i⟩) }}
+  [{
+    [1| ld in `{(0,0)}   => sum x_mval x_midx x_colind x_rowptr];
+    {2| ld in `[0, Nrow] \x `[0, Ncol] => get x_mval x_midx x_colind x_rowptr ld.1 ld.2}
+  }]
+  {{ hv, \[hv[`1]((0,0)) = Σ_(i <- `[0, Nrow] \x `[0, Ncol]) hv[`2](i)] \* \Top}}.
+Proof with (try seclocal_fold; seclocal_solver).
+demo.
+  (* xin (2,0) : do 3 (xwp; xapp).
+  xin (1,0) : xwp; xapp=> s...
+  rewrite prod_cascade.
+  set (R i := 
+    arr(x_mval, mval)⟨2, i⟩ \*
+    arr(x_colind, colind)⟨2, i⟩ \* 
+    arr(x_rowptr, rowptr)⟨2, i⟩ : hhprop D).
+  set (Inv (i : int) := 
+    arr(x_mval, mval)⟨1, (0,0)⟩ \* 
+    arr(x_colind, colind)⟨1, (0,0)⟩ \* 
+    arr(x_rowptr, rowptr)⟨1, (0,0)⟩).
+  xfor_sum Inv R R (fun hv i => Σ_(j <- `{i} \x `[0, Ncol]) hv[`2](j)) s.
+  { rewrite /Inv /R.
+    xin (2,0) : rewrite wp_prod_single /=.
+    xin (1,0) : do 3 (xwp; xapp)...
+    xframe2 (arr(x_rowptr, rowptr)⟨1, (0, 0)⟩).
+    xsubst (snd : _ -> int).
+    xnapp sv.sum_spec...
+    { move=> ?/in_interval_list... }
+    move=> ?; rewrite -wp_equiv. xsimpl=>->.
+    xapp @incr.spec.
+    unfold prod; rewrite -SumCascade ?Sum1 -?SumCascade; try by disjointE.
+    erewrite SumEq with (fs:=`[0, Ncol]). 1: xsimpl*.
+    move=>* /=; by rewrite Sum1. }
+  { xapp. xsimpl*. rewrite SumCascade //; disjointE. } *)
+Qed.
+
+
+End unordered_csr.
+
+End unordered_csr.
